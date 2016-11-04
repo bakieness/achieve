e3cfdf8ce25e0d308ad82241855ede6af73c1e9e
bakieness <BAKJ5_11@uni.worc.ac.uk>
2016-11-04 17:04:52 +0000

    Add files via upload

---
+++ js/app.js
@@ -0,0 +1,55 @@
+﻿'use strict';
+
+/* App Module */
+var myApp = angular.module('myApp', ['ngRoute', 'controllers', 'firebase'])
+.constant('FIREBASE_URL', 'https://ipet.firebaseio.com/');
+
+myApp.run(['$rootScope', '$location',
+    function ($rootScope, $location) {
+        $rootScope.$on('$routeChangeError', function (event, next, previous, error) {
+            if (error == 'AUTH_REQUIRED') {
+                $rootScope.message = "Sorry, you must login in to access this page";
+                $location.path('/login');
+            }
+        })
+}]);
+
+myApp.config(['$routeProvider',
+  function ($routeProvider) {
+    $routeProvider.
+        when('/index', {
+          templateUrl: 'views/home.html',
+          controller: 'RegistrationCtrl'
+        }).
+        when('/profile', {
+          templateUrl: 'views/profile.html',
+          controller: 'RegistrationCtrl'
+        }).
+        when('/games', {
+          templateUrl: 'views/games.html',
+          controller: 'RegistrationCtrl'
+        }).
+        when('/achievements', {
+          templateUrl: 'views/achievements.html',
+          controller: 'RegistrationCtrl'
+        }).
+        otherwise({
+          redirectTo: '/index'
+        });
+}]);
+
+
+//Copyright (c) 2013 Justin Klemm
+myApp.filter('orderObjectBy', function () {
+    return function (items, field, reverse) {
+        var filtered = [];
+        angular.forEach(items, function (item) {
+            filtered.push(item);
+        });
+        filtered.sort(function (a, b) {
+            return (a[field] > b[field] ? 1 : -1);
+        });
+        if (reverse) filtered.reverse();
+        return filtered;
+    };
+});
---
+++ js/header.js
@@ -0,0 +1,20 @@
+﻿$(document).ready(function(){
+    //mobile navigation toggle button
+    $(".menu").click(function(){
+        $(".menu").toggleClass("rotate");
+
+        if($('.nav').css("height") == '0px'){
+            //$(".content").animate({'margin-top': '100'}, 200);
+            $('.nav').animate({ 'height': '126' }, 200);
+        }
+        else{
+            //$(".content").animate({'margin-top': '5'}, 200);
+            $('.nav').animate({ 'height': '0' }, 200);
+        }
+    });
+
+    //on mobile navigation close menu
+    $(".primary").click(function(){
+        $(".menu").removeClass("rotate");
+    });
+});
